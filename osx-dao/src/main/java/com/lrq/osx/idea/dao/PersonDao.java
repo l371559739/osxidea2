d6e2128b20551b1d0ad93ea2a6d7ae3b78174335
lrq <371559739@qq.com>
2015-10-04 15:17:32 +0800

    lrq commit at 20151004

--- osx-dao/src/main/java/com/lrq/osx/idea/dao/PersonDao.java
+++ osx-dao/src/main/java/com/lrq/osx/idea/dao/PersonDao.java
@@ -6,10 +6,18 @@ import com.lrq.osx.idea.entity.Person;
  * Created by lrq on 15/10/4.
  */
 public class PersonDao {
+    public Person getPerson(String personId){
+        return new Person();
+    }
+
     public Person addPerson(Person p){
         return p;
     }
+
     public boolean delPerson(Person p){
         return false;
     }
+
+
+
 }
